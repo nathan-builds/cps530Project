ae1cf2574a476c35750dae9728690b40ac57ac3d
Sulaxan <sulaxan33@yahoo.ca>
2021-12-01 17:02:41 -0500

    add references

--- frontend/src/pages/Page6.js
+++ frontend/src/pages/Page6.js
@@ -4,7 +4,8 @@ const Page6 = () => {
   return (
     <div className="outer-container">
       <div className="container">
-        <h1>Nathaniel Hans</h1>
+        <h1 className="section-title">Team Members</h1>
+        <h2>Nathaniel Hans</h2>
         <ul>
             <li>Installed and configured React</li>
             <li>Setup React Routing for pages</li>
@@ -13,7 +14,7 @@ const Page6 = () => {
             <li>React installation guide</li>
             <li>Creating a Python interpreter: React tutorial</li>
         </ul>
-        <h1>Sulaxan Pius</h1>
+        <h2>Sulaxan Pius</h2>
         <ul>
             <li>Installed and configured Flask</li>
             <li>Setup backend endpoints for Python code execution</li>
@@ -22,6 +23,22 @@ const Page6 = () => {
             <li>Flask installation guide</li>
             <li>Creating a Python interpreter: Flask tutorial</li>
         </ul>
+
+        <h1 className="section-title">References</h1>
+        <section>
+          <ul>
+            <li>https://www.statista.com/statistics/1124699/worldwide-developer-survey-most-used-frameworks-web/</li>
+            <li>https://www.simform.com/blog/websites-use-react/</li>
+            <li>https://dev.to/detimo/python-flask-pros-and-cons-1mlo</li>
+            <li>https://www.stxnext.com/blog/flask-vs-django-comparison/</li>
+            <li>https://www.javatpoint.com/pros-and-cons-of-react</li>
+            <li>https://github.com/pallets/flask</li>
+            <li>https://github.com/django/django</li>
+            <li>https://dev.to/detimo/python-flask-pros-and-cons-1mlo</li>
+            <li>https://www.stxnext.com/blog/flask-vs-django-comparison/</li>
+            <li>https://dev.to/amigosmaker/what-is-flask-used-for-2do5</li>
+          </ul>
+        </section>
       </div>
     </div>
   );
