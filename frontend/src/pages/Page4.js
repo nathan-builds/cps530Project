b32349351cb7947b3cd3073317cfe04786360087
Nathan Hans <n.hans@simfront.com>
2021-12-01 14:21:24 -0500

    Working editor

--- frontend/src/pages/Page4.js
+++ frontend/src/pages/Page4.js
@@ -1,35 +1,57 @@
-import axios from 'axios'
-import "./page4.css"
+import axios from "axios";
+import "./page4.css";
 import { useState } from "react";
 
-const Page4 =()=>{
-    const [textAreaValue, setTextAreaValue] =useState();
+const Page4 = () => {
+  const [textAreaValue, setTextAreaValue] = useState("");
+  const [result, setResult] = useState("");
 
-    const onKeyEnteredHandler=(event)=>{
-        setTextAreaValue(event.target.value)
-    }
+  const onKeyEnteredHandler = (event) => {
+    setTextAreaValue(event.target.value);
+  };
 
+  async function onSubmitHandler() {
+    let testObj = {
+      Code: textAreaValue,
+    };
+    console.log(textAreaValue);
 
-    const onSubmitHandler=()=>{
-        console.log(textAreaValue)
-        let testObj={
-            Code: textAreaValue
-        };
+    // fetch("http://localhost:5000/flask/hello", {
+    //   method: "POST",
+    //   headers: { "Content-type": "application.json" },
+    //   body: JSON.stringify(testObj),
+    // }).then(result => result.json()).then(result)
 
-        fetch('http://localhost:5000/flask/hello',{
-        method:'POST',
-        headers:{'Content-type': 'application.json'},
-        body:JSON.stringify(testObj)
-         })
-        }
+    const postDetails = {
+      method: "POST",
+      headers: { "Content-type": "application.json" },
+      body: JSON.stringify(testObj),
+    };
+    const response = await fetch(
+      "http://localhost:5000/flask/hello",
+      postDetails
+    );
+    const data = await response.json();
 
-    return (
-    <div >
-        <textarea id="codeArea" className="code-area" onKeyPress= {onKeyEnteredHandler}>Type here</textarea>
-        <button type = "submit" onClick = {onSubmitHandler}>Click me</button>
+    setResult(data.result);
+  }
+
+  return (
+    <div>
+      <textarea
+        id="codeArea"
+        className="code-area"
+        onChange={onKeyEnteredHandler}
+        value={textAreaValue}
+      >
+        Type here
+      </textarea>
+      <button type="submit" onClick={onSubmitHandler}>
+        Click me
+      </button>
+      <textarea value={result}></textarea>
     </div>
-   );
-    
-}
+  );
+};
 
 export default Page4;
