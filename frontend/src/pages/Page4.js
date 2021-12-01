d74c8c59fd1500c61223ded99cc8bd8b10244f12
Nathan Hans <n.hans@simfront.com>
2021-12-01 13:22:18 -0500

    Text Area Added

--- frontend/src/pages/Page4.js
+++ frontend/src/pages/Page4.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
+import "./page4.css"
+import { useState } from "react";
 
 const Page4 =()=>{
+    const [textAreaValue, setTextAreaValue] =useState();
+
+    const onKeyEnteredHandler=(event)=>{
+        setTextAreaValue(event.target.value)
+    }
+
 
     const onSubmitHandler=()=>{
+        console.log(textAreaValue)
         let testObj={
-            Id: "Hello",
-            Text: "World",
+            Code: textAreaValue
         };
 
         fetch('http://localhost:5000/flask/hello',{
@@ -17,8 +25,8 @@ const Page4 =()=>{
 
     return (
     <div >
+        <textarea id="codeArea" className="code-area" onKeyPress= {onKeyEnteredHandler}>Type here</textarea>
         <button type = "submit" onClick = {onSubmitHandler}>Click me</button>
-        <textarea>Type here</textarea>
     </div>
    );
     
