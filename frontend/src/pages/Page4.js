f30da500eb1cccc912dcbaeae9b6d77bf80745aa
Nathan Hans <n.hans@simfront.com>
2021-12-01 13:03:21 -0500

    Test API

--- frontend/src/pages/Page4.js
+++ frontend/src/pages/Page4.js
@@ -1,5 +1,27 @@
+import axios from 'axios'
+
 const Page4 =()=>{
-    return (<h1>This is Page 4</h1>);
+
+    const onSubmitHandler=()=>{
+        let testObj={
+            Id: "Hello",
+            Text: "World",
+        };
+
+        fetch('http://localhost:5000/flask/hello',{
+        method:'POST',
+        headers:{'Content-type': 'application.json'},
+        body:JSON.stringify(testObj)
+         })
+        }
+
+    return (
+    <div >
+        <button type = "submit" onClick = {onSubmitHandler}>Click me</button>
+        <textarea>Type here</textarea>
+    </div>
+   );
+    
 }
 
 export default Page4;
