a8a014d5192f74c09c2f1f59b3cfbdb5f61f3bf7
Sulaxan <sulaxan33@yahoo.ca>
2021-12-01 13:45:44 -0500

    execute requested code

--- api/python_eval_handler.py
+++ api/python_eval_handler.py
@@ -1,28 +1,23 @@
 from flask import request
 from flask_restful import Api, Resource, reqparse
 
+from io import StringIO
+from contextlib import redirect_stdout
+
 class PythonEvalHandler(Resource):
   def post(self):
     json = request.get_json(force=True)
 
-    id = json["Id"]
-    text = json["Text"]
-
-    print(json)
-    # note, the post req from frontend needs to match the strings here (e.g. 'type and 'message')
-
-    request_type = id
-    request_json = text
-    # ret_status, ret_msg = ReturnData(request_type, request_json)
-    # currently just returning the req straight
-    ret_status = request_type
-    ret_msg = request_json
-
-    if ret_msg:
-      message = "message: {}".format(ret_msg)
-    else:
-      message = "no message"
+    code = json["Code"]
     
-    final_ret = {"status": "success", "message": message}
+    capture = StringIO()
+
+    with redirect_stdout(capture):
+      compiled_code = compile(code, 'internal_code_run', 'exec')  # exec allows us to execute code blocks
+      exec(compiled_code)
+
+    output = capture.getvalue()
+
+    final_ret = {"status": "success", "result": output}
 
     return final_ret
