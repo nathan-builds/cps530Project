54e12d51e9a69496d1b463abc6cadad1d5c310d5
Nathan Hans <n.hans@simfront.com>
2021-12-01 16:53:41 -0500

    p6

--- frontend/src/pages/Page5.js
+++ frontend/src/pages/Page5.js
@@ -1,5 +1,49 @@
-const Page5 =()=>{
-    return (<h1>This is Page 5</h1>);
-}
+import "./page.css";
+
+const Page5 = () => {
+  return (
+    <div className="outer-container">
+      <div className="container">
+        <h1> Summary</h1>
+        <p>
+          Overall the experience of using Flask and React went fairly well.
+          Flask was easy to install and configure making it optimal for a small
+          project such as this. Being able to easily define REST endpoints
+          within the Flask architecture allowed us to quickly link our React
+          frontend with our backend. Using the npm package manager made
+          installing and creating the React frontend very simple; this process
+          took about five minutes. Configuring links to each of the different
+          pages took a little more work due to the fact that React apps use the
+          Single Page Application (SPA) architecture. 
+          </p>
+          <p>
+          The learning curve to
+          learn both Flask and React wasn't too steep, as what was required to
+          get a working website up and running wasn't too complex and, instead,
+          quite straightforward. The code we wrote felt like an extension to
+          what we did when we learned HTML/CSS/JavaScript/PHP, and that there
+          was a lot more abstraction and various (straightforward) concepts to
+          (1) make it easier to create a website, and (2) to be able to do more
+          powerful things. 
+          </p>
+          <p>
+          We would not do things differently in retrospect, as
+          we're quite happy with how our end product turned out. However, if we
+          were to go back to this project, and assuming we had more time, we'd
+          definitely make this project more feature filled as it has quite a bit
+          of potential.
+          </p>
+          
+          <p>
+           Flask and React are both great frameworks, and
+          definitely solid choices we'd choose for future projects. Thus, we
+          have no regrets in the frameworks we chose, and mostly have positive
+          things to say about them.
+          </p>
+    
+      </div>
+    </div>
+  );
+};
 
 export default Page5;
--- frontend/src/pages/Page6.js
+++ frontend/src/pages/Page6.js
@@ -1,5 +1,21 @@
-const Page6 =()=>{
-    return (<h1>This is Page 6</h1>);
-}
+import "./page.css";
+
+const Page6 = () => {
+  return (
+    <div className="outer-container">
+      <div className="container">
+        <h1>Nathaniel Hans</h1>
+        <ul>
+            <li>Installed and configured React</li>
+            <li>Setup React Routing for pages</li>
+            <li>Handle the user input and POST method to backend</li>
+            <li>Handle response from the backend</li>
+        </ul>
+        <h1>Sulaxan Pius</h1>
+        <p></p>
+      </div>
+    </div>
+  );
+};
 
 export default Page6;
