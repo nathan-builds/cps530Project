120437a8d739459f4043a8295855e551f7b985d8
Sulaxan <sulaxan33@yahoo.ca>
2021-12-01 16:59:40 -0500

    add responsibilities

--- frontend/src/pages/Page6.js
+++ frontend/src/pages/Page6.js
@@ -10,9 +10,18 @@ const Page6 = () => {
             <li>Setup React Routing for pages</li>
             <li>Handle the user input and POST method to backend</li>
             <li>Handle response from the backend</li>
+            <li>React installation guide</li>
+            <li>Creating a Python interpreter: React tutorial</li>
         </ul>
         <h1>Sulaxan Pius</h1>
-        <p></p>
+        <ul>
+            <li>Installed and configured Flask</li>
+            <li>Setup backend endpoints for Python code execution</li>
+            <li>Implemented execution of Python code</li>
+            <li>Styling for interpreter</li>
+            <li>Flask installation guide</li>
+            <li>Creating a Python interpreter: Flask tutorial</li>
+        </ul>
       </div>
     </div>
   );
