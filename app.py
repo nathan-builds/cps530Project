f47b4b0b61f02dff6ef2d640688f5d47d69d8660
Sulaxan <sulaxan33@yahoo.ca>
2021-12-03 11:47:34 -0500

    add flask routes

--- app.py
+++ app.py
@@ -11,11 +11,48 @@ api = flask_restful.Api(app)
 
 @app.route('/', defaults={'path': ''})
 @cross_origin()
-
 def serve(path):
     return flask.send_from_directory(app.static_folder, 'index.html')
 
+
+@app.route('/page1', defaults={'path': ''})
+@cross_origin()
+def servePage1(path):
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/page2', defaults={'path': ''})
+@cross_origin()
+def servePage2(path):
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/page3', defaults={'path': ''})
+@cross_origin()
+def servePage3(path):
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/page4', defaults={'path': ''})
+@cross_origin()
+def servePage4(path):
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/page5', defaults={'path': ''})
+@cross_origin()
+def servePage5(path):
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/page6', defaults={'path': ''})
+@cross_origin()
+def servePage6(path):
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+
 api.add_resource(PythonEvalHandler, '/pyinterpreter/eval')
+
 
 if __name__ == '__main__':
     app.run()
